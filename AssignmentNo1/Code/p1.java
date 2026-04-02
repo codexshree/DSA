7e2647196e05abadaf7ef236075fcdde347ec6c4
codexshree <swapnillohar02@gmail.com>
2026-04-02 11:08:14 +0530

    Add files via upload

---
+++ AssignmentNo1/Code/p1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class P1{
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter range : ");
+		int n = sc.nextInt();
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
